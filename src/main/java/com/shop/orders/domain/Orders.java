075b07d377c1a445ccc1150e4ee16c2f5b713e8d
couldwhite <75570282+couldwhite@users.noreply.github.com>
2021-03-23 14:42:16 +0400

    Create entities

---
+++ src/main/java/com/shop/orders/domain/Orders.java
@@ -0,0 +1,21 @@
+package com.shop.orders.domain;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String departureCity;
+    private String arrivedCity;
+    private Date createdOrderTime;
+    private Date departureTime;
+    private Date arrivedTime;
+    private Date closedOrderTime;
+}
